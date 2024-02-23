d70f91be4e74f7386a1942c3969d56e14a939536
JoshuaDragon <97946335+JoshuaDragon@users.noreply.github.com>
2024-02-23 12:09:05 +0200

    Add files via upload

---
+++ DWA_14 Challenge Solutions/components/tally-header.js
@@ -0,0 +1,25 @@
+import { LitElement, html, css } from '../libs/lit-html.js ';
+
+class TallyHeader extends LitElement {
+    static styles = css`
+    header {
+        display: flex;
+        margin: 10px;
+        justify-content: center;
+      }
+      h1 {
+        font-size: 3rem;
+        color: white
+      }
+    `;
+
+    render() {
+        return html`
+        <header>
+        <h1> <i class="fa-solid fa-up-down fa-lg"></i> Tally Counter </h1>
+    </header>
+    `;
+    }
+};
+
+customElements.define('tally-header', TallyHeader);
